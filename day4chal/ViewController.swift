9dc24a5ecbff831ac285b51ef0a2de28d19719d3
sarakim13 <sarakim0903@gmail.com>
2019-06-27 23:23:07 -0400

    done

--- day4chal/ViewController.swift
+++ day4chal/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let password  = "kim"
+
+    @IBOutlet weak var txt: UITextField!
+    
+    @IBOutlet weak var answer: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
-}
+    
+    @IBAction func submitbutton(_ sender: UIButton) {
+        var userInput = txt.text
+        if userInput == password {
+            answer.text = "you got it"
+        }
+        else{
+            answer.text = "try it again"
+        }
+    }
+    
+} 
 
